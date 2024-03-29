4581c427a88c527f9ff7864fe63286cfcb959e76
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-03-29 15:26:43 +0100

    Initial Commit

---
+++ CourseProject/CourseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseProjectApp.swift
+//  CourseProject
+//
+//  Created by Karsten Verherstraeten on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CourseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
